4683676852f460fa4eadfca9ca5d2451cbdeb656
InCastleTrue <fedx4467@naver.com>
2023-11-24 17:38:08 +0900

    Animal Movement & Animal Recognize Reaction
    동믈 움직임 및 동물이 레이를 받아 각각의 종류에 따라 리액션을 다르게 합니다.

--- EscapeGameProject/Assets/Scripts/AnimalMovement.cs
+++ EscapeGameProject/Assets/Scripts/AnimalMovement.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+
+public enum MonsterBehavior
+{
+    Aggression,
+    Defensiveness
+}
+
+
 
 public class AnimalMovement : MonoBehaviour
 {
+
     private Animation ani;
     public float moveSpeed = 5f;
     public float rotationSpeed = 45f;
@@ -12,17 +22,110 @@ public class AnimalMovement : MonoBehaviour
 
     private float timer = 0f;
 
+    public MonsterBehavior behavior;
+    public float rayRange = 10f;
+    public Transform player;
+    public bool isPlayerCheck;
+
+    public float coneAngle = 45f;
+    public int rayCount = 5;
+
+
+    private void Start()
+    {
+        ani = GetComponent<Animation>();
+
+    }
+
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= rotationInterval)
+
+
+        if (!isPlayerCheck)
         {
-            RotateSmoothly();
-            
-            timer = 0f;
+
+            ani.wrapMode = WrapMode.Loop;
+            ani.Play("walk_1");
+
+            timer += Time.deltaTime;
+            if (timer >= rotationInterval)
+            {
+                RotateSmoothly();
+
+                timer = 0f;
+            }
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+            RaycastHit hit;
+            float raySpacing = coneAngle / (rayCount - 1);
+
+            for (int i = 0; i < rayCount; i++)
+            {
+                float currentHorizontalAngle = -coneAngle / 2 + i * raySpacing;
+
+                for (int j = 0; j < rayCount; j++)
+                {
+                    float currentVerticalAngle = -coneAngle / 2 + j * raySpacing;
+
+                    Quaternion rayRotation = Quaternion.Euler(currentVerticalAngle, currentHorizontalAngle, 0);
+
+                    if (Physics.Raycast(this.gameObject.transform.position, rayRotation * this.gameObject.transform.forward, out hit, rayRange))
+                    {
+                        if (hit.collider.CompareTag("Player"))
+                        {
+                            isPlayerCheck = true;
+                            break;
+                        }
+                    }
+                    Debug.DrawRay(transform.position, rayRotation * transform.forward * rayRange, Color.Lerp(Color.red, Color.green, (i * rayCount + j) / (float)(rayCount * rayCount - 1)));
+                }
+
+            }
+
         }
 
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        else if (isPlayerCheck)
+        {
+            ani.wrapMode = WrapMode.Loop;
+            ani.Play("run");
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+            if (behavior == MonsterBehavior.Aggression)
+            {
+                Vector3 directionToPlayer = (player.position - transform.position);
+                directionToPlayer.y = 0f;
+
+                Quaternion rotation = Quaternion.LookRotation(directionToPlayer);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 2f);
+
+                if (distanceToPlayer > 1f)
+                {
+                    transform.Translate(Vector3.forward * moveSpeed * 2 * Time.deltaTime);
+                }
+                else if (distanceToPlayer >= 20)
+                {
+                    isPlayerCheck = false;
+                }
+
+            }
+
+            if (behavior == MonsterBehavior.Defensiveness)
+            {
+                Vector3 directionToPlayer = (player.position - transform.position);
+                directionToPlayer.y = 0f;
+
+                Quaternion rotation = Quaternion.LookRotation(-directionToPlayer);
+                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * 2f);
+
+                transform.Translate(Vector3.forward * moveSpeed * 2 * Time.deltaTime);
+                if (distanceToPlayer >= 20)
+                {
+                    isPlayerCheck = false;
+                }
+            }
+        }
+
     }
 
     void RotateSmoothly()
@@ -44,5 +147,11 @@ public class AnimalMovement : MonoBehaviour
             yield return null;
         }
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerCheck = true;
+        }
+    }
 }
-
