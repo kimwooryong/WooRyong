b19cc665a676a2589dc215964f7ea979a7f5697b
kimwooryong <141311537+kimwooryong@users.noreply.github.com>
2023-11-28 18:44:52 +0900

    제작창UI, 크로스레이 제작
    제작창 UI를 인벤토리와 비슷하게 만들었으며 아이템 인식을 위한 레이 만들었습니다.

--- EscapeGameProject/Assets/KimWooRyong/CraftManual.cs
+++ EscapeGameProject/Assets/KimWooRyong/CraftManual.cs
@@ -68,6 +68,7 @@ public class CraftManual : MonoBehaviour
 
     void Update()
     {
+        RayObject(); // 게임 오브젝트 레이 확인용
         if (go_Preview != null)
         {
             savedRotation = go_Preview.transform.rotation;
@@ -84,10 +85,49 @@ public class CraftManual : MonoBehaviour
         }
     }
 
+    public float maxRaycast; // 오브젝트 확인 레이 거리 
+    public GameObject crosshairPrefab; // 크로스헤어 프리팹
+    private GameObject crosshairInstance; // 크로스헤어
+
+    private void RayObject() // 은하씨 드릴 레이 아이템 확인용
+    {
+        Debug.DrawRay(Camera.main.transform.position + Camera.main.transform.forward, Camera.main.transform.forward, Color.blue); // 씬 창에 레이선이 보이게 해주는 코드
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward); // 카메라 기준 가운데에 레이 발사
+
+        RaycastHit hit; // 맞은거
+
+        if (Physics.Raycast(ray, out hit, maxRaycast))
+        {
+            Debug.Log("맞은 오브젝트 " + hit.collider.gameObject.name); // 레이에 충돌한 오브젝트의 이름을 표시
+
+            crosshairPrefab.gameObject.SetActive(true);
+
+            Vector3 crosshairPosition = hit.point; // 크로스헤어를 표시할 위치 설정
+
+            UpdateCrosshair(crosshairPosition); // 크로스헤어를 표시하거나 업데이트
+        }
+        else
+        {
+            Debug.Log("HideCrosshair");
+            crosshairPrefab.gameObject.SetActive(false);
+        }
+    }
+    void UpdateCrosshair(Vector3 position)
+    {
+        // 크로스헤어를 표시할 위치에 인스턴스를 생성하거나 위치 업데이트
+        if (crosshairInstance == null)
+        {
+            crosshairInstance = Instantiate(crosshairPrefab, position, Quaternion.identity);
+        }
+        else
+        {
+            crosshairInstance.transform.position = position;
+        }
+    }
 
     private void InputHandler()
     {
-        if (Input.GetKeyDown(KeyCode.T) && !isPreviewActived) // UI열기인데 중복열림 방지
+        if (Input.GetKeyDown(KeyCode.T) && !isPreviewActived) // UI열기 중복열림 방지
         {
             Window(); // UI
         }
