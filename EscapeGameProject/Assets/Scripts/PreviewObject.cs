00be40a277ff695361e4c40e61874918227ee37a
jmpark <rladnfyd1311@naver.com>
2023-12-04 13:12:05 +0900

    고기
    다시 올려봅니다

--- EscapeGameProject/Assets/Scripts/PreviewObject.cs
+++ EscapeGameProject/Assets/Scripts/PreviewObject.cs
@@ -23,7 +23,6 @@ public class PreviewObject : MonoBehaviour
     public List<Renderer> _renderers = new List<Renderer>();
     public List<GameObject> _gameObjects = new List<GameObject>();
 
-    public Vector3 BuildObjectSize = new Vector3(2.5f, 2.5f, 2.5f);
 
     public void Start() 
     {
@@ -92,6 +91,7 @@ public class PreviewObject : MonoBehaviour
     {
         return colliderList.Count == 0;
     }
+
 
     public virtual Vector3 GetSnapPosition(Vector3 currentPosition, Transform snapTransform) // 위치 설정
     {
