d17943e20db7acad9785befbc1813cf991b2321b
jmpark <rladnfyd1311@naver.com>
2023-12-05 09:26:17 +0900

    스크립트 오류
    스크립트 사라진 오류 수정

---
+++ EscapeGameProject/Assets/Scripts/SnapObject.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapObject : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
