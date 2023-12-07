b910ca1f6ab75ddf134c0cdbbdd1e31071355fa1
InCastleTrue <fedx4467@naver.com>
2023-12-07 09:20:47 +0900

    BushUpdate

---
+++ EscapeGameProject/Assets/Scripts/CampFire.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampFire : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
