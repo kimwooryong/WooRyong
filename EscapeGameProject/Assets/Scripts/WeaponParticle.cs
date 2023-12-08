0ffc9de241c25cbc8c4b47e782a9e627ba49002c
InCastleTrue <fedx4467@naver.com>
2023-12-08 14:13:06 +0900

    Tag Add

--- EscapeGameProject/Assets/Scripts/WeaponParticle.cs
+++ EscapeGameProject/Assets/Scripts/WeaponParticle.cs
@@ -2,22 +2,36 @@ using UnityEngine;
 
 public class WeaponParticle : MonoBehaviour
 {
-    public GameObject particlePrefab;
+    public GameObject[] particlePrefab;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Weapon") || other.CompareTag("Hammer") || other.CompareTag("Pickaxe"))
+        if (other.CompareTag("Animal"))
         {
             Vector3 hitPoint = other.ClosestPointOnBounds(transform.position);
-            GameObject particleInstance = Instantiate(particlePrefab, hitPoint, Quaternion.identity);
+            GameObject particleInstance = Instantiate(particlePrefab[0], hitPoint, Quaternion.identity);
+            Invoke("DestroyParticle", 0.5f);
+        }
+        else if (other.CompareTag("Tree"))
+        {
+            Vector3 hitPoint = other.ClosestPointOnBounds(transform.position);
+            GameObject particleInstance = Instantiate(particlePrefab[1], hitPoint, Quaternion.identity);
+            Invoke("DestroyParticle", 0.5f);
+        }
+        else if (other.CompareTag("Rock"))
+        {
+            Vector3 hitPoint = other.ClosestPointOnBounds(transform.position);
+            GameObject particleInstance = Instantiate(particlePrefab[2], hitPoint, Quaternion.identity);
             Invoke("DestroyParticle", 0.5f);
         }
     }
 
     private void DestroyParticle()
     {
-        GameObject particleInstance = GameObject.FindWithTag("Particle");
-        if (particleInstance != null)
+
+        GameObject[] particles = GameObject.FindGameObjectsWithTag("Particle");
+
+        foreach (GameObject particleInstance in particles)
         {
             Destroy(particleInstance);
         }
