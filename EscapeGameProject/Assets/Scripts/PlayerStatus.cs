315513dbabba57999d743cceef1142a14865a0e0
InCastleTrue <148035149+InCastleTrue@users.noreply.github.com>
2023-12-09 23:35:00 +0900

    .

--- EscapeGameProject/Assets/Scripts/PlayerStatus.cs
+++ EscapeGameProject/Assets/Scripts/PlayerStatus.cs
@@ -239,8 +239,9 @@ public class PlayerStatus : MonoBehaviour
             yield return new WaitForSeconds(1.0f); // Adjust the interval as needed
         }
     }
-    public void AddHp(int amount)
+    public void AddHunger(int amount)
     {
-        playerCurrentHp += amount;
+        theCurrentStateOfHunger += amount;
     }
+    
 }
