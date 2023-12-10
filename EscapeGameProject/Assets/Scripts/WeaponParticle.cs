4dca0c587710c3af35e0f7ed184baac882d6aa84
InCastleTrue <148035149+InCastleTrue@users.noreply.github.com>
2023-12-10 18:52:27 +0900

    Attack Once
    휘두를 때 공격이 한번만 들어가게 설정

--- EscapeGameProject/Assets/Scripts/WeaponParticle.cs
+++ EscapeGameProject/Assets/Scripts/WeaponParticle.cs
@@ -2,24 +2,43 @@ using UnityEngine;
 
 public class WeaponParticle : MonoBehaviour
 {
-
     public GameObject[] particlePrefab;
     private PlayerStatus player;
     private PlayerCotroller playerController;
-
+    private CapsuleCollider capsuleCollider;
+    private bool hasPlayedAttackMissSound = false; // 새 변수
 
     private void Start()
     {
         player = GetComponentInParent<PlayerStatus>();
         playerController = GetComponentInParent<PlayerCotroller>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        capsuleCollider.enabled = false;
     }
 
+    private void Update()
+    {
+        if (playerController.isAttacking)
+        {
+            capsuleCollider.enabled = true;
+
+            if (!hasPlayedAttackMissSound)
+            {
+                SoundManager.Instance.PlayPlayerAttackMiss();
+                hasPlayedAttackMissSound = true;
+            }
+        }
+        else
+        {
+            capsuleCollider.enabled = false;
+            hasPlayedAttackMissSound = false;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (gameObject.CompareTag("Hammer"))
         {
-            //if로 무기들 tag 해두고 PlayerStatus에 데미지를 나눠놓고 거기에 맞게 더해주기
             player.animalDamage = 2;
             player.rockDamage = 2;
             player.TreeDamage = 4;
@@ -36,7 +55,8 @@ public class WeaponParticle : MonoBehaviour
             player.rockDamage = 4;
             player.TreeDamage = 2;
         }
-        if (other.CompareTag("Animal") && playerController._isAttack)
+
+        if (other.CompareTag("Animal"))
         {
             AnimalMovement animal = other.GetComponent<AnimalMovement>();
             if (animal != null)
@@ -45,8 +65,14 @@ public class WeaponParticle : MonoBehaviour
 
                 if (boxCollider != null)
                 {
+                    int beforeHp = animal.currentHp;
+                    SoundManager.Instance.PlayPlayerAttackTree(); // 다른 소리로 바꾸자
                     animal.isHit = true;
                     animal.TakeDamage(player.animalDamage);
+                    if (animal.currentHp != beforeHp)
+                    {
+                        playerController.isAttacking = false;
+                    }
                     Vector3 hitPoint = boxCollider.ClosestPointOnBounds(transform.position);
                     GameObject particleInstance = Instantiate(particlePrefab[0], hitPoint, Quaternion.identity);
                     Invoke("DestroyParticle", 0.5f);
@@ -54,7 +80,7 @@ public class WeaponParticle : MonoBehaviour
             }
         }
       
-        else if (other.CompareTag("Tree") && playerController._isAttack)
+        else if (other.CompareTag("Tree"))
         {
             Tree tree = other.GetComponent<Tree>();
             if (tree != null)
@@ -63,15 +89,20 @@ public class WeaponParticle : MonoBehaviour
 
                 if (meshCollider != null)
                 {
-
-                    tree.TakeDamage(player.animalDamage);
+                    int beforeHp = tree.currentHp;
+                    SoundManager.Instance.PlayPlayerAttackTree();
+                    tree.TakeDamage(player.TreeDamage);
+                    if (tree.currentHp != beforeHp) 
+                    {
+                        playerController.isAttacking = false;
+                    }
                     Vector3 hitPoint = meshCollider.ClosestPointOnBounds(transform.position);
                     GameObject particleInstance = Instantiate(particlePrefab[1], hitPoint, Quaternion.identity);
                     Invoke("DestroyParticle", 0.5f);
                 }
             }
         }
-        else if (other.CompareTag("Rock") && playerController._isAttack)
+        else if (other.CompareTag("Rock"))
         {
             Rock rock = other.GetComponent<Rock>();
             if (rock != null)
@@ -80,14 +111,20 @@ public class WeaponParticle : MonoBehaviour
 
                 if (capsuleCollider != null)
                 {
-
-                    rock.TakeDamage(player.animalDamage);
+                    int beforeHp = rock.currentHp;
+                    SoundManager.Instance.PlayPlayerAttackRock();
+                    rock.TakeDamage(player.rockDamage);
+                    if (rock.currentHp != beforeHp)
+                    {
+                        playerController.isAttacking = false;
+                    }
                     Vector3 hitPoint = capsuleCollider.ClosestPointOnBounds(transform.position);
                     GameObject particleInstance = Instantiate(particlePrefab[2], hitPoint, Quaternion.identity);
                     Invoke("DestroyParticle", 0.5f);
                 }
             }
         }
+
     }
 
     private void DestroyParticle()
