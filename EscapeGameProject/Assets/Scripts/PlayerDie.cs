56ab4818b2790e6a4f81c9dae8a4809467743bcd
InCastleTrue <148035149+InCastleTrue@users.noreply.github.com>
2023-12-11 00:40:42 +0900

    Player Die UI & Animal Fix & SavePoint Fix

---
+++ EscapeGameProject/Assets/Scripts/PlayerDie.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDie : MonoBehaviour
+{
+    private GameManager gameManager;
+    private Transform firstSpawnPosition;
+    private UIManager uiManager;
+    private PlayerCotroller playerCotroller;
+    private PlayerStatus player;
+
+    private bool isFading = false;
+
+    public float fadeDuration = 5f;
+    public CanvasGroup panelCanvasGroup;
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        uiManager = FindObjectOfType<UIManager>();
+        playerCotroller = FindObjectOfType<PlayerCotroller>();
+        player = FindObjectOfType<PlayerStatus>();
+        if (gameManager != null)
+        {
+            firstSpawnPosition = gameManager.spawnPlace.transform;
+        }
+    }
+
+    void Update()
+    {
+        if (player == null && playerCotroller == null)
+        {
+            player = FindObjectOfType<PlayerStatus>();
+        }
+
+        if (!isFading && player != null && player.playerCurrentHp <= 0)
+        {
+            StartCoroutine(FadePanel());
+        }
+    }
+
+    public IEnumerator FadePanel()
+    {
+        isFading = true;
+
+        panelCanvasGroup.gameObject.SetActive(true);
+        float timer = 0f;
+        player.rb.isKinematic = true;
+        while (timer < fadeDuration)
+        {
+            panelCanvasGroup.alpha = Mathf.Lerp(0f, 1f, timer / fadeDuration);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(player.gameObject);
+        panelCanvasGroup.alpha = 0f;
+        panelCanvasGroup.gameObject.SetActive(false);
+
+        isFading = false;
+        
+    }
+}
--- EscapeGameProject/Assets/Scripts/UIManager.cs
+++ EscapeGameProject/Assets/Scripts/UIManager.cs
@@ -8,17 +8,25 @@ public class UIManager : MonoBehaviour
     public GameObject sleepPanel;
     public Button sleepYesButton;
     public Button sleepNoButton;
-    private SavePoint savePoint;
-
+    public SavePoint savePoint;
 
     private void Start()
     {
         sleepPanel.SetActive(false);
-        savePoint = FindObjectOfType<SavePoint>();
         sleepYesButton.onClick.AddListener(OnSleepYesButton);
         sleepNoButton.onClick.AddListener(OnSleepNoButton);
-
+        StartCoroutine(UpdateSavePointCoroutine());
     }
+
+    private IEnumerator UpdateSavePointCoroutine()
+    {
+        while (savePoint == null)
+        {
+            savePoint = FindObjectOfType<SavePoint>();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
     private void OnSleepYesButton()
     {
         GameManager.Instance.InvisibleCursor();
@@ -26,13 +34,11 @@ public class UIManager : MonoBehaviour
         sleepPanel.SetActive(false);
         Time.timeScale = 1.0f;
     }
+
     private void OnSleepNoButton()
     {
         sleepPanel.SetActive(false);
         GameManager.Instance.InvisibleCursor();
         Time.timeScale = 1.0f;
     }
-
-
-
 }
